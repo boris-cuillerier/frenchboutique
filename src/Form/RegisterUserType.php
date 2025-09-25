6615fba17601969b61ce523732295afedc461c09
Boris Cuillerier <boris.cuillerier@gmail.com>
2025-09-25 18:24:43 +0200

    password validator and user fields constraints

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,13 +3,16 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
@@ -22,20 +25,49 @@ class RegisterUserType extends AbstractType
                     'placeholder' => 'votre@email.com'
                 ]
             ])
-            ->add('password', PasswordType::class, [
-                'label' => "Votre mot de passe",
-                'attr' => [
-                    'placeholder' => 'Nouveau mot de passe'
-                ]
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 4,
+                        'max' => 30
+                    ])
+                ],
+                'first_options'  => [
+                    'label' => 'Votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Nouveau mot de passe'
+                    ], 
+                    'hash_property_path' => 'password'
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez le mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmation du mot de passe'
+                    ]                    
+                ],
+                'mapped' => false
             ])
             ->add('firstname', TextType::class, [
                 'label' => "Votre prénom",
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'max' => 30
+                    ])
+                ],
                 'attr' => [
                     'placeholder' => 'Prénom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => "Votre nom",
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'max' => 30
+                    ])
+                ],
                 'attr' => [
                     'placeholder' => 'Nom'
                 ]
@@ -52,6 +84,12 @@ class RegisterUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'constraints' => [
+                new UniqueEntity([
+                    'entityClass' => User::class,
+                    'fields' => 'email'
+                ])
+            ],
             'data_class' => User::class,
         ]);
     }
