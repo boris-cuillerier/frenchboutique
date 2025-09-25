138bff3b6cc63b0c6e6174ec55751341127eee9c
Boris Cuillerier <boris.cuillerier@gmail.com>
2025-09-25 13:26:27 +0200

    Persist user data in DB

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
+        $form->handleRequest($request);
 
-        $form = $this->createForm(RegisterUserType::class);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
 
         return $this->render('register/index.html.twig', [
             'registerform' => $form->createView()
